f342cbff95f7099c9d3a51b1469ce6f4c10b26f0
Conor Tierney <G00425686@atu.ie>
2023-02-07 15:00:19 +0000

    week2task

---
+++ bank.py
@@ -0,0 +1,13 @@
+# pythonbank.py
+# Author: Conor Tierney
+# weekly task 02
+
+amt1 = int(input("enter amount1(in cents):"))
+print (f"the amount is {amt1}")
+
+amt2 = int(input("enter amount2(in cents):"))
+print (f"the amount is {amt2}")
+
+amttotal  = (amt1 + amt2)/100
+
+print (f"the sum of these is: €{amttotal}")
