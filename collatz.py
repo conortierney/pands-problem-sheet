597bcef82c4d106d5159e01b7a1c1515a344cc0c
G00425686 <G00425686@atu.ie>
2023-04-02 16:26:23 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,4 +1,5 @@
-#Task 4: collatz.py
+#collatz.py
+# Week 4 task
 #Author : Conor Tierney.
 
 # This program asks the user to input a positive integer stored as "number".
@@ -7,35 +8,17 @@
 # if number is odd, x n by 3 and add 1.
 # when number becomes 1, the program ends.
 
-#referneces: W3schools, realpython.com (whileloops), 
+#ref: W3schools, realpython.com (whileloops), 
 #ref: AUTOMATE THE BORING STUFF WITH PYTHON - flow control.
 
 
-n = int(input("Please enter a positive integer: "))   #ask user to input a positive number.
+number = int(input("Please enter a positive integer: "))     # ask user to input a positive number.
 
-while n != 1:                                         #while number is not equal to 1,
-    print(n, end=" ")                                 #print the value of the number.
-    if n % 2 == 0:                                    #if number is even,
-        n = n // 2                                    # program divides number by 2.
-    else:                                             #if number is odd,
-        n = 3 * n + 1                                 # program multiplies number by 3 and add 1
+while number != 1:                                           # while number is not equal to 1,
+    print(number, end=" ")                                   # print the value of the number.
+    if number % 2 == 0:                                      # if number is even,
+        number = number // 2                                      # program divides number by 2.
+    else:                                               # else: if number is odd,
+        number = 3 * number + 1                                   # program multiplies number by 3 and add 1
 
-print(n,end=" ")                                      #when the number =1, program prints final value.  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(number,end=" ")                                        # when the number =1, program prints final value.  
