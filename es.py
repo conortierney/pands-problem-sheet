7f98a3f30b233852d20c777f645727e34b5c8cb3
conorti13 <G00425686@atu.ie>
2023-04-02 15:06:38 +0100

    week7task

--- es.py
+++ es.py
@@ -1,18 +1,20 @@
-# week 7 task 
-# Author: Conor Tierney
-# count 'e's in text task
-# # Program to get letter count in a text file.
-# reference: www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+# week 7 task, es.py
+# Author: Conor Tierney.
+# This program reads th the text file 'count_es.txt' and outputs the number of e's it contains.
+# Program to get letter count in a text file.
+# Ref: www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+# Ref: https://realpython.com/working-with-files-in-python/
+# Ref: https://www.w3schools.com/python/python_file_handling.asp
 
 FILENAME = "count_es.txt"
 
 
-with open("count_es.txt", "r") as f:                 # opens the file name & path for reading.
+with open("count_es.txt", "r") as f:                     # opens the file name & path for reading.
     
     contents = f.read()                                  # Reads the contents of the file.
 
-count = contents.count("e")                               # Use count to get the number of times the letter 'e' is in text.
+count = contents.count("e")                              # Use count to get the number of times the letter 'e' is in text.
 
-print(f"This file contains the letter 'e' {count} times.")
+print(f"The file 'count_es.txt' contains the letter 'e' {count} times.")
 
 # I used the text "Hello there. My name is Conor."  # This text has 4 'e's in it.
