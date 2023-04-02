aed5dd268bc27487ae5708f641e7127253a5a275
conorti13 <G00425686@atu.ie>
2023-04-02 15:48:44 +0100

    update  wk6 task

--- squareroot.py
+++ squareroot.py
@@ -1,33 +1,34 @@
 # squareroot.py
-# week 6 task
-# This program prompts the user to enter a positive number as input,
-# and outputs an approximation of its squareroot.
+# Week 6 task.
 # Author: Conor Tierney
+# This program prompts the user to enter a positive number as input.
+# and outputs an approximation of its squareroot using the newton method at estimating square roots.
+# Ref: https://tutorialsinhand.com/Articles/python-program-to-find-square-root-of-a-number-using-newton-square-root-formula
+# Ref: https://www.youtube.com/watch?v=xdlIFw5EM4w
+
+
 
 def sqrt():                                                   # define (def) a new function for square root.
     
     while True:                                               # while loop to ensure number entered is positive.
         try:
-            x = float(input("Please enter a positive number: "))     # Prompt the user to enter a positive number
+            x = float(input("Please enter a positive number: "))     # Prompt the user to enter a positive number.
             if x > 0:
                 break                                         # while loop stops if user enters positive number.
             else:
-                print("incorrect,the number must be positive")     # else: print error message.
+                print("incorrect,the number must be positive")     # else: print error message if negative number or incorrect input.
         except ValueError:
             print("incorrect input,enter a positive number")
-                                                       
-    sqroot = x / 2                                                    # initial guess of sqroot for the positive number.                                        
-     
+
+                                        
+    approx = x / 2                                                    # assume approx as half the number.                                        
     while True:
         
-        new_guess = 0.5 * (sqroot + x / sqroot)                       # estimate square root using newton method.
+        better = 0.5 * (approx + x / approx)                       # find a better value using newton method.
         
+        if abs(better - approx) < 1e-9:
+            return better
         
-        if abs(new_guess - sqroot) < 1e-9:
-            return new_guess
-        
-        
-        sqroot = new_guess                                       # update the program for the new guess
+        approx = better                                            # assume founfd better value as approx.
 
-
-print(f"The square root of this is: {sqrt()}")                  # output the result in a string format for result.
+print(f"The square root of this is approx: {sqrt()}")                  # output the result in a string format for result.
