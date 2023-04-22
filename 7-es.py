74d2b7472a72078ae7525ce699da436def083b51
G00425686 <G00425686@atu.ie>
2023-04-22 15:06:40 +0100

    Update 7-es.py

--- 7-es.py
+++ 7-es.py
@@ -6,6 +6,8 @@
 # Ref: https://realpython.com/working-with-files-in-python/
 # Ref: https://www.w3schools.com/python/python_file_handling.asp
 
+# CODE UPDATE TO COUNT FOR BOTH CAPITAL AND SMALL E'S.
+
 FILENAME = "count_es.txt"
 
 
@@ -13,8 +15,11 @@ with open("count_es.txt", "r") as f:                     # opens the file name &
     
     contents = f.read()                                  # Reads the contents of the file.
 
-count = contents.count("e")                              # Use count to get the number of times the letter 'e' is in text.
+count_e = contents.count("e")                              # Use count to get the number of times the letter 'e' is in text.
+count_E = contents.count("E")
+count_total = count_e + count_E
 
-print(f"The file 'count_es.txt' contains the letter 'e' {count} times.")
+print(f"The file 'count_es.txt' contains the letter 'e' and 'E' {count_total} times.")   # count small and capital e's
 
-# I used the text "Hello there. My name is Conor."  # This text has 4 'e's in it.
+# I used the text "HELLO there. My name is Conor."  # This text has 4 'e's in it in total.
+
